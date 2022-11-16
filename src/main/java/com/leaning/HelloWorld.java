a3de8b0682d1e244486feb6d0377078cb1c7d478
Kirill Chetverikov <MaraZmOo@yandex.ru>
2022-11-16 20:53:12 +0300

    last

--- src/main/java/com/leaning/HelloWorld.java
+++ src/main/java/com/leaning/HelloWorld.java
@@ -1,9 +1,109 @@
 package com.leaning;
 
 public class HelloWorld {
-
-
     public static void main(String[] args) {
+        //Задан массив из чисел, где все числа встречаются четное количество раз, и только одно число - нечетное количество раз.
+        // Вычислить это число
+
+        int[] array = {2, 2, 2, 2, 1, 3, 1};
+        System.out.println(searchNumber(array));
+    }
+
+    static int searchNumber(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int check = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j])
+                    check++;
+            }
+            if (check % 2 != 0)
+                return array[i];
+        }
+        return -1;
+    }
+
+
+    public static void main9(String[] args) {
+        //Метод должен посчитать разницу между минимальным и максимальным значениями элементов массива целых чисел
+        int[] array = {5, 11, 2, 16, 1, 3, 100};
+
+        System.out.println(toValue(array));
+    }
+
+    static int toValue(int[] array) {
+        int minValue = array[0];
+        int maxValue = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            } else if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        System.out.println("max = " + maxValue);
+        System.out.println("min = " + minValue);
+        return maxValue - minValue;
+    }
+
+    public static void main8(String[] args) {
+        //F(n) - значение n-ного числа Фибоначчи
+        //Требуется реализовать методы для итеративного и рекурсивного вычисления F(n)
+        //Указать алгоритмическую сложность и сложность по памяти
+
+
+        fib(6); //Olog(3)
+        System.out.println(fibRec(4));
+    }
+
+    static int fib(int n) {
+        int f1 = 0;
+        int f2 = 1;
+        for (int i = 2; i <= n; i++) {
+            int value = f1 + f2;
+            f1 = f2;
+            f2 = value;
+        }
+        System.out.println("value = " + f2);
+        return n;
+
+        // Olog(n)
+
+
+    }
+
+    static int fibRec(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibRec(n - 1) + fibRec(n - 2);
+        }
+        // Olog(n)
+    }
+
+    public static void main7(String[] args) {
+        //int x = ...;
+        //int y = ...;
+        //Требуется поменять местами значения переменных, то что лежало в x поместить в y, и наоборот. Запрещено пользоваться буферной переменной
+
+        int x = 2;
+        int y = 3;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        x = x + y; // 5
+        y = x - y; // 2
+        x = -y + x; //3
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+
+    }
+
+    public static void main6(String[] args) {
         System.out.println(factorial(6));
         System.out.println(factorialRec(6));
     }
@@ -47,6 +147,7 @@ public class HelloWorld {
     static long sum(int x, long y) {
         return x + y;
     }
+
     static void incrementAndPrint(int x) {
         x++;
         System.out.println(x);
@@ -65,7 +166,7 @@ public class HelloWorld {
                 if (result % 2 == 0) {
                     continue;
                 }
-                System.out.println("i = " + i + " j = " + j + " result "  + result);
+                System.out.println("i = " + i + " j = " + j + " result " + result);
             }
         }
     }
@@ -91,8 +192,8 @@ public class HelloWorld {
     }
 
     public static void main2(String[] args) {
-        int x ;
-       // System.out.println(x);
+        int x;
+        // System.out.println(x);
         x = 10; //init
         x = 20; //assing
         System.out.println("Hello world");
